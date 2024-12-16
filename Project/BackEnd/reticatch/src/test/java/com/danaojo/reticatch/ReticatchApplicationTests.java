931ccc8c7dec970b1ac7b006aa7eecbd867d30c0
sw2park <pswoo990803@gmail.com>
2024-12-16 15:34:11 +0900

    24.12.16 first commit

---
+++ Project/BackEnd/reticatch/src/test/java/com/danaojo/reticatch/ReticatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danaojo.reticatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReticatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
